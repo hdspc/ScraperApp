deced60fff5b571fcbd65fd4cfdfc60c3a4862de
hdspc <105600018+hdspc@users.noreply.github.com>
2024-01-14 15:23:11 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+
+namespace SeleniumDocs.Hello;
+
+public static class HelloSelenium
+{
+    public static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+
+        driver.Navigate().GoToUrl("https://www.retrojunk.com/commercials?page=1&sortColumn=DateAdded&sortOrder=Desc&decade=1990");
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+        string title = driver.Title;
+
+        IList<IWebElement> elements = driver.FindElements(By.ClassName("grid-item"));
+        foreach (IWebElement e in elements)
+        {
+            System.Console.WriteLine(e.Text);
+            //e.Click();
+
+        }
+        driver.SwitchTo().NewWindow(WindowType.Tab);
+
+        Console.WriteLine(title.ToString());
+        Console.WriteLine("boner");
+
+        //gridBox.Click();
+        // driver.Quit();
+    }
+}
