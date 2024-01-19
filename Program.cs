6a2cbc1b1611b4eeeeb1a363836da1acd1a767eb
hdspc <105600018+hdspc@users.noreply.github.com>
2024-01-19 10:51:30 -0800

    Now cycling through pages, errors are lessening but still not bulletproof. Waits don't seem 100%

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Collections.Generic;
 
 
 namespace SeleniumDocs.Hello;
@@ -12,72 +9,62 @@ public static class HelloSelenium
     public static void Main()
     {
 
-        var service = ChromeDriverService.CreateDefaultService();
-
-        int pageNum = 1;
-
-       
-
-
         var chromeOptions = new ChromeOptions();
         chromeOptions.AddArgument("--test-type");
         chromeOptions.AddExcludedArguments("excludeSwitches", "enable-logging");
-
         IWebDriver driver = new ChromeDriver(chromeOptions);
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
 
         string title = driver.Title;
 
-        driver.Manage().Window.Minimize();
-        driver.Navigate().GoToUrl($"https://www.retrojunk.com/commercials?page={pageNum}&sortColumn=DateAdded&sortOrder=Desc&decade=1990");
-       
 
-
-        try
-
+        for (int pageNum = 1; pageNum < 10; pageNum++)
         {
-            IList<IWebElement> imageLinks = driver.FindElements(By.ClassName("img-wrap"));
-
-            loopMethod(imageLinks);
-            pageNum++;
             driver.Navigate().GoToUrl($"https://www.retrojunk.com/commercials?page={pageNum}&sortColumn=DateAdded&sortOrder=Desc&decade=1990");
 
+            try
+
+            {
+                IList<IWebElement> imageLinks = driver.FindElements(By.ClassName("img-wrap"));
+
+                loopMethod(imageLinks, pageNum);
+
+
+            }
+
+            catch (StaleElementReferenceException ex)
+
+            {
+                Console.WriteLine("exception caught" + ex.ToString());
+            }
+
+            Console.WriteLine("Test");
+            
         }
 
-        catch (StaleElementReferenceException ex)
-       
+        driver.Close();
+
+        void loopMethod(IList<IWebElement> elementList, int pageNumber)
+
         {
-            Console.WriteLine("exception caught"+ ex.ToString());
-        }
-
-        Console.WriteLine(title.ToString());
-        Console.WriteLine("Test");
-
-        // driver.Quit();
-
-         void loopMethod(IList<IWebElement> elementList)
-       
-                {
-          
-
-
             // Create a StreamWriter to write to a text file
             using (StreamWriter writer = new StreamWriter("image_urls.txt", true)) // Open the file in append mode
-                    {
-                        writer.WriteLine(DateTime.Now.ToString());
+            {
+                Console.WriteLine("page number = " + pageNumber.ToString());
 
-                        foreach (IWebElement e in elementList)
-                        {
-                    System.Console.WriteLine("page number = " + pageNum.ToString());
-                           // System.Console.WriteLine(e.GetAttribute("href"));
-                           writer.WriteLine(e.GetAttribute("href"));
-                        }
-                        Console.WriteLine("Image URLs from page " + driver.Url + " have been saved to image_urls.txt");
-                    }
+                foreach (IWebElement e in elementList)
+                {
+                    Console.WriteLine(title.ToString());
+
+                    Console.WriteLine(e.GetAttribute("href"));
+                    writer.WriteLine(e.GetAttribute("href"));
+                }
+                Console.WriteLine("Image URLs from page " + driver.Url + " have been saved to image_urls.txt");
+            }
 
 
-               }
+        }
 
     }
 
